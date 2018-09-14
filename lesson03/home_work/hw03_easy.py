a3026c68a1bb486adea14cf469d72b870bb4848d
DoctorChe <duncan.reg@yandex.ru>
2018-09-14 08:57:11 +0300

    hw03_easy is done

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,8 +4,26 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
 def my_round(number, ndigits):
-    pass
+    delimiter = str(number).find(".")
+    number_str_tmp = ""
+    number_str = str(number)[:delimiter + ndigits + 1]
+    digit_last = int(str(number)[delimiter + ndigits + 1])
+
+    if digit_last >= 5:
+        count = len(number_str)
+        extra = True
+        while count > 0:
+            if number_str[count - 1] != ".":
+                digit_tmp = str(int(number_str[count - 1]) + extra)
+                extra = True if len(digit_tmp) > 1 else False
+                number_str_tmp = digit_tmp[-1] + number_str_tmp
+            else:
+                number_str_tmp = "." + number_str_tmp
+            count -= 1
+
+    return float(number_str_tmp)
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +38,13 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    first = str(ticket_number)[:len(str(ticket_number)) // 2]
+    second = str(ticket_number)[len(str(ticket_number)) // 2:]
+
+    first_sum = sum(map(int, first))
+    second_sum = sum(map(int, second))
+
+    return first_sum == second_sum
 
 
 print(lucky_ticket(123006))
