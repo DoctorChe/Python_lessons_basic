c47a1958712c8335e586f05fbdf0d142784af02a
DoctorChe <doctor_che@inbox.com>
2018-09-15 01:24:49 +0300

    hw04_normal is done

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,27 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+import re
+
+pattern = re.compile('[A-Z]*([a-z]+)[A-Z]*')
+list_1 = re.findall(pattern, line)
+print(list_1)
+
+
+list_1 = []
+s = ""
+upper_ch = False
+for ch in line:
+    if ch.islower():
+        s += ch
+    if ch.isupper() and len(s) > 0:
+        upper_ch = True
+        list_1.append(s)
+        s = ""
+if len(s) > 0 and upper_ch:
+    list_1.append(s)
+print(list_1)
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +66,48 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+pattern = re.compile('[a-z]{2}([A-Z]+)[A-Z]{2}')
+list_2 = re.findall(pattern, line_2)
+print(list_2)
+
+list_2 = []
+s = ""
+for ch in line_2:
+    if ch.islower():
+        if len(s) < 2:
+            s += ch
+        elif len(s) >= 5:
+            list_2.append(s[2:-2])
+            s = ch
+        elif s[-1].isupper():
+            s = ch
+        else:
+            s = s[1:] + ch
+    if ch.isupper():
+        if len(s) >= 2:
+            s += ch
+        else:
+            s = ""
+print(list_2)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+
+with open("data.txt", "w", encoding='UTF-8') as f:
+    f.write("".join(str(random.randint(0, 9)) for _ in range(2500)))
+
+with open("data.txt", "r", encoding='UTF-8') as f:
+    data = f.read()
+
+pattern = re.compile('(1+)|(2+)|(3+)|(4+)|(5+)|(6+)|(7+)|(8+)|(9+)|(0+)')
+lst = re.findall(pattern, data)
+flat_list = [item for sublist in lst for item in sublist if item != ""]
+max_len_seq = str(max(list(map(int, flat_list))))
+print(f"Самая длинная последовательность одинаковых цифр в вышезаполненном файле: {max_len_seq}")
